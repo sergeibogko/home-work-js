c5ebae94997e219916bccfd46e73a41fd868f1c2
sergeibogko <69677370+sergeibogko@users.noreply.github.com>
2020-11-01 17:32:54 +0200

    Правильно сделанная Задача 2 в Модуле 1

--- js/module1.2.js
+++ js/module1.2.js
@@ -1,10 +1,13 @@
-const invoice = 100; 
+const invoice = 200;
 const stock = 100;
 
 // Write code under this line
-const message = ;
+const message =
+  invoice <= 100
+    ? 'Заказ оформлен, с вами свяжется менеджер'
+    : 'На складе недостаточно товаров!';
 
-//console.log(message);
+// console.log(message);
 
 //если invoice равен 100
 // то значение message будет равно
@@ -12,8 +15,8 @@ const message = ;
 
 //если invoice равен 50
 // то значение message будет равно
-// 'Заказ оформлен, с вами свяжется менеджер'  
+// 'Заказ оформлен, с вами свяжется менеджер'
 
 //если invoice равен 150
 // то значение message будет равно
-// 'На складе недостаточно товаров!'  
+// 'На складе недостаточно товаров!'
