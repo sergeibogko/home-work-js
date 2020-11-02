eec16e1513d682f17fdc0736e1dfc7dc80b23724
sergeibogko <69677370+sergeibogko@users.noreply.github.com>
2020-11-02 23:17:18 +0200

    Правильно сделанная задача 4 в модуле 1

--- js/module1.4.js
+++ js/module1.4.js
@@ -1,17 +1,24 @@
-const orderPieces = 4;
+const orderPieces = 0;
 
 const credits = 23580;
 const pricePerDroid = 3000;
 const CANCELED_BY_USER = 'Отменено пользователем!';
 const ACCESS_DENIED = 'Недостаточно средств на счету!';
 
-let totalPrice; // Write code on this line
-let balanceCredit; // Write code on this line
+let totalPrice = orderPieces * pricePerDroid; // Write code on this line
+let balanceCredit = credits - totalPrice; // Write code on this line
 let message;
 
 // Write code under this line
+if (orderPieces === null) {
+  message = CANCELED_BY_USER;
+} else if (orderPieces >= 0 && orderPieces < 8) {
+  message = `Вы купили ${orderPieces} дроидов, на счету осталось ${balanceCredit} кредитов`;
+} else if (totalPrice > balanceCredit) {
+  message = ACCESS_DENIED;
+}
 
-// console.log(message);
+console.log(message);
 
 //если orderPieces равно 4
 // то значение message будет равно
